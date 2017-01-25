9fb6366fa5b296e5883c36ce588241920a0419f5
Elife Ozturk <elife.042@gmail.com>
2017-01-25 16:51:02 +0300

    İlk git commit

---
+++ MiningGraph/coin.py
@@ -0,0 +1,5 @@
+### Elife Ozturk #####
+### Github usage with Pycharm https://www.youtube.com/watch?v=bsN7ogDz02g
+### MiningGraph algorithm is implemented from "Mining Networks with Shared Items" on Facebook Dataset
+######################
+
---
+++ MiningGraph/mocap.py
@@ -0,0 +1,4 @@
+### Elife OZTURK #####
+### Aim is Mining Most Common Attribute Patterns from Attributed Graphs
+### MOst Common Attribute Patterns (MOCAP)
+#####################################################
